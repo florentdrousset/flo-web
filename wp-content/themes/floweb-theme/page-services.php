bc545a08fc2115836faad4a44959310087e5f4c0
Florent Drousset <florent.drousset@gmail.com>
2021-07-31 18:01:38 +0200

    services page WIP

--- wp-content/themes/floweb-theme/page-services.php
+++ wp-content/themes/floweb-theme/page-services.php
@@ -17,11 +17,39 @@ get_header(); ?>
     <!-- LAST ARTICLES -->
     <div class="articles m-auto mt-8 w-1/2">
         <article>
-            <h2 class="2xl:font-mono text-3xl font-bold text-black dark:text-yellow-200">Here is a subtitle</h2>
+            <h2 class="2xl:font-mono text-3xl font-bold text-black dark:text-yellow-200">Présentation</h2>
             <hr class="border-2 border-black dark:border-gray-700 border-solid mb-5">
-            <p class="mb-3">
-                <?php echo the_content(); ?>
-            </p>
+            <article class="mb-3">
+                Je m'appelle Florent, et je suis un développeur fullstack spécialisé dans WordPress. Avant de me lancer en freelance, j'ai travaillé à plein temps en tant que développeur web back-end dans une ESN.
+
+                J'ai bien conscience du fait qu'un site est souvent modifié par plusieurs personnes sur de nombreuses années, et c'est pourquoi je suis particulièrement attentif au fait de produire du code <strong>compréhensible</strong> et facilement <strong>réutilisable</strong>.
+
+                <p>Que vous ayez pour projet de créer un <strong>site de e-commerce</strong> ou un site vitrine pour votre activité, je peux vous aider.</p>
+
+                <h2 class="2xl:font-mono text-3xl font-bold text-black dark:text-yellow-200 mt-4">Mes compétences</h2>
+                <hr class="border-2 border-black dark:border-gray-700 border-solid mb-5">
+                <article class="flex mt-5">
+                    <div><img src="<?=BASE_FOLDER?>/assets/icons/mentalite.png"/><h5 class="font-bold">Solution clé en main</h5><p>Du code de qualité, moderne et réutilisable.</p></div>
+                    <div><img src="<?=BASE_FOLDER?>/assets/icons/mentalite.png"/><h5 class="font-bold">Code propre</h5><p>Du code de qualité, moderne et réutilisable.</p></div>
+                    <div><img src="<?=BASE_FOLDER?>/assets/icons/mentalite.png"/><h5 class="font-bold">Code propre</h5><p>Du code de qualité, moderne et réutilisable.</p></div>
+                    <div><img src="<?=BASE_FOLDER?>/assets/icons/mentalite.png"/><h5 class="font-bold">Code propre</h5><p>Du code de qualité, moderne et réutilisable.</p></div>
+                </article>
+
+                <h2 class="2xl:font-mono text-3xl font-bold text-black dark:text-yellow-200 mt-4">Tâches courantes</h2>
+                <hr class="border-2 border-black dark:border-gray-700 border-solid mb-5">
+                <p>Voici quelques unes des tâches auxquelles je suis régulièrement confronté :</p>
+                <br/>
+                <div class="bg-blue-400 bg-opacity-60 rounded border-opacity-60 border-2 border-blue-700 p-4 pl-8">
+                    <ul class="list-disc">
+                        <li>Ajouter une nouvelle fonctionnalité sur votre site web, que ce soit en développant un plugin à partir de 0 ou en étendant un plugin déjà existant.</li>
+                        <li>Créer un nouveau thème pour votre site ou en modifier un déjà existant.</li>
+                        <li>Mettre en place des formulaires.</li>
+                        <li>Débugger ou trouver une solution à un problème technique que rencontre votre site.</li>
+                    </ul>
+                </div>
+
+
+            </article>
 
         </article>
 
